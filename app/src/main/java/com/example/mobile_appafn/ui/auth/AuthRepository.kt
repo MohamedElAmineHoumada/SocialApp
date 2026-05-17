2cab58cdc2db026e0232a1bfcf90d2420fb8ee57
Fatima Zohra <mbou45643@gmail.com>
2026-05-17 14:23:30 +0100

    Suppression ancien projet mobile_appafn

--- app/src/main/java/com/example/mobile_appafn/ui/auth/AuthRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mobile_appafn.ui.auth
-
-class AuthRepository {
-    fun login(email: String, password: String) {}
-}
--- app/src/main/java/com/example/mobile_appafn/ui/auth/RegisterViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.mobile_appafn.ui.auth
-
-class RegisterViewModel {
-}
