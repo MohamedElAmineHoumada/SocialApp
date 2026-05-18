1844ff928cacda1a697fcb7da47c27e04b5a3792
Fatima Zohra <mbou45643@gmail.com>
2026-05-18 18:01:13 +0100

    Resoultion probleme de blockage

--- app/src/main/java/com/Groupe15/SocialApp/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/Groupe15/SocialApp/ui/feed/FeedFragment.kt
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.Groupe15.SocialApp.R
-
 class FeedFragment : Fragment(R.layout.fragment_feed) {
 
     private val viewModel: FeedViewModel by viewModels()
